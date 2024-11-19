f9c0b0342713389b94de86df91791b8fe0c4acfd
FemboyIRL <gatoelluis@gmail.com>
2024-11-18 20:13:09 -0600

    user has seen wizard bugfix

--- lib/utilities/data_stored/wizard_presentation_data.dart
+++ lib/utilities/data_stored/wizard_presentation_data.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 abstract class WizardPresentationData {
   static Future<bool> hasSeenWizard() async {
     final prefs = await SharedPreferences.getInstance();
-    return false;
+    return prefs.getBool("USER_HAS_SEEN_WIZARD") == true;
   }
 
   static Future<void> setUserHasSeenTheWizard(final bool value) async {
